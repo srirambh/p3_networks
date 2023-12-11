8cd4666753d6de63901d782f921fe2804ad5503d
Sriram Bharadwaj <sriram@Srirams-MacBook-Pro.local>
2023-12-10 19:16:09 -0600

    changed trace.py

--- trace.py
+++ trace.py
@@ -1,27 +1,57 @@
-import argparse 
+import argparse
 import socket
 import struct
 import sys
-from collections import defaultdict
+from datetime import datetime
 
 def receiveRes(sock):
     header = None
     try:
         rec = sock.recvfrom(10000)[0]
-        header = struct.unpack_from(f"!cI4sH4sH", rec)
+        header = struct.unpack_from("!cI4sH4sH", rec)
         return header
     except:
         return header
-
 
 def printPacketInfo(srcIP, srcPort, destIP, destPort, ttl):
     print("TTL: ", ttl)
     print("Source IP: ", srcIP)
     print("Source Port: ", srcPort)
-    print("Dest IP: ", destIP)
-    print("Dest Port: ", destPort)
+    print("Destination IP: ", destIP)
+    print("Destination Port: ", destPort)
     print("=====================")
 
+def traceRoute(src_ip, src_port, dest_ip, dest_port, debug_option):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((socket.gethostname(), src_port))
+    sock.settimeout(5)
+
+    ttl = 0
+    path = []
+    while True:
+        packet = struct.pack(f"!cI4sH4sH", b'T', ttl, src_ip, src_port, dest_ip, dest_port)
+        sock.sendto(packet, (socket.inet_ntoa(src_ip), src_port))
+        if debug_option == 1:
+            print("=====Sent Packet====")
+            printPacketInfo(socket.inet_ntoa(src_ip), src_port, socket.inet_ntoa(dest_ip), dest_port, ttl)
+
+        res = receiveRes(sock)
+        if not res:
+            print("Can't reach node")
+            sys.exit()
+
+        path.append((socket.inet_ntoa(res[2]), res[3]))
+        if debug_option == 1:
+            print("=====Received Packet====")
+            printPacketInfo(socket.inet_ntoa(res[2]), res[3], socket.inet_ntoa(res[4]), res[5], ttl)
+
+        if res[2] == dest_ip and res[3] == dest_port:
+            print("Destination reached")
+            for i in range(len(path)):
+                print(f"Hop {i+1} IP {path[i][0]} Port {path[i][1]}")
+            break
+        else:
+            ttl += 1
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
@@ -33,38 +63,7 @@ if __name__ == "__main__":
     p.add_argument("-f", "--debug_option", help="Input debug options")
     args = p.parse_args()
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((socket.gethostname(), int(args.routetrace_port)))
-    sock.settimeout(5) 
+    src_ip = socket.inet_aton(socket.gethostbyname(args.source_hostname))
+    dest_ip = socket.inet_aton(socket.gethostbyname(args.destination_hostname))
 
-    selfIP = socket.inet_aton(socket.gethostbyname(socket.gethostname()))
-    srcIP = socket.inet_aton(socket.gethostbyname(args.source_hostname))
-    destIP = socket.inet_aton(socket.gethostbyname(args.destination_hostname))
-    
-    
-    ttl = 0
-    path = []
-    while True:
-        packet = struct.pack(f"!cI4sH4sH", b'T', ttl, srcIP, int(args.routetrace_port), destIP, int(args.destination_port))
-        sock.sendto(packet, (socket.inet_ntoa(srcIP), int(args.source_port)))
-        if(int(args.debug_option) == 1):
-            print("=====Sent Packet====")
-            printPacketInfo(socket.inet_ntoa(srcIP), int(args.source_port), socket.inet_ntoa(destIP), int(args.destination_port), ttl)
-            
-        res = receiveRes(sock)
-        if(not res):
-            print("Can't reach node")
-            sys.exit()
-
-        path.append((socket.inet_ntoa(res[2]),res[3]))
-        if(int(args.debug_option) == 1):
-            print("=====Received Packet====")
-            printPacketInfo(socket.inet_ntoa(res[2]), res[3], socket.inet_ntoa(res[4]), res[5], ttl)
-
-        if(res[2] == destIP and res[3] == int(args.destination_port)):
-            print("Destination reached")
-            for i in range(len(path)):
-                print(f"Hop {i+1} IP {path[i][0]} Port {path[i][1]}")
-            break
-        else:
-            ttl += 1
+    traceRoute(src_ip, int(args.source_port), dest_ip, int(args.destination_port), int(args.debug_option))
